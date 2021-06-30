27cc2c47e640a218a252ddf2f491ba79e624cdc6
MehdiAghighi <mamaliosezar@gmail.com>
2021-07-01 01:36:26 +0430

    added Weight Conversion

--- src/UnitConversionsClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace MehdiAghighi\UnitConversions;
-
-class UnitConversionsClass
-{
-}
---
+++ tests/WeightTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MehdiAghighi\UnitConversions\Tests;
+
+use MehdiAghighi\UnitConversions\Weight;
+use PHPUnit\Framework\TestCase;
+
+class WeightTest extends TestCase
+{
+    /** @test */
+    public function can_convert_kilograms_to_lbs()
+    {
+        $lbs = Weight::fromKilograms( 100 )->toLbs();
+        $this->assertEquals( 220.46, $lbs );
+    }
+}
